cb93b65ba2ffd64c0711023fc4f547e5da807e13
Ryan Abraham <ryan_ja@live.ca>
2017-01-14 20:56:13 -0500

    First working version after refactoring

--- Scripts/Clash.asm
+++ Scripts/Clash.asm
@@ -1,15 +1,18 @@
 .include "Scripts/header.inc"
 .include "Scripts/snes_init.asm"
 
+;MAP_WIDTH: db $5
+;MAP_HEIGHT: db $5
+
 .macro ConvertX
 ; Data in: our coord in A
 ; Data out: SNES scroll data in C (the 16 bit A)
 .rept 5
-asl a		; multiply A by 32
+asl a			; multiply A by 32
 .endr
 rep #%00100000	; 16 bit A
-eor #$FFFF	; this will do A=1-A
-inc a		; A=A+1
+eor #$FFFF		; this will do A=1-A
+inc a			; A=A+1
 sep #%00100000	; 8 bit A
 .endm
 
@@ -17,17 +20,17 @@ sep #%00100000	; 8 bit A
 ; Data in: our coord in A
 ; Data out: SNES scroll data in C (the 16 bit A)
 .rept 5
-asl a		; multiply A by 32
+asl a			; multiply A by 32
 .endr
 rep #%00100000	; 16 bit A
-eor #$FFFF	; this will do A=1-A
+eor #$FFFF		; this will do A=1-A
 sep #%00100000	; 8 bit A
 .endm
 
+;--------------------------------------
 .bank 0 slot 0
 .org 0
 .section "Vblank"
-;--------------------------------------
 ;;;;;;;;
 ;VBLANK;
 ;;;;;;;;
@@ -52,13 +55,12 @@ bne +			; if not B, jump
 ; B has been pressed
 ; do stuff
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;JOYPAD MOVEMENT CHECKS;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 +
 lda $0201		; get joypad input
-and #$00001111	; AND input with control stick inputs
+and #%00001111	; AND input with control stick inputs
 sta $0201		; store the AND result
 
 ; NOTE: (0,0) is top-left of map
@@ -75,7 +77,7 @@ lda $0201 		; get stored joypad input
 cmp #%00000100  ; down?
 bne +			; if not down, jump
 lda $0101		; get player y-value
-cmp MAP_HEIGHT	; player at bottom of map?
+cmp #$05		; player at bottom of map?
 beq +			; if at bottom, jump
 inc $0101		; move player 1 down
 
@@ -86,17 +88,197 @@ bne +			; if not left, jump
 lda $0100		; get player x-value
 cmp #$00		; player at left of map?
 beq +			; if at far left, jump
-dec $0101		; move player 1 left
+dec $0100		; move player 1 left
 
 +
 lda $0201 		; get stored joypad input
 cmp #%00000001  ; right?
 bne +			; if not right, jump
 lda $0100		; get player x-value
-cmp #$MAP_WIDTH	; player at left of map?
+cmp #$05		; player at left of map?
 beq +			; if at far right, jump
-dec $0101		; move player 1 right
+inc $0100		; move player 1 right
 
 +
 rti 			; done joypad input checks
 .ends			; done VBlank
+;--------------------------------------
+
+
+
+
+.bank 0 slot 0
+.org 0
+.section "Main"
+;--------------------------------------
+Start:
+ Snes_Init
+rep #%00010000	;16 bit xy
+sep #%00100000	;8 bit ab
+
+ldx #$0000
+- lda UntitledPalette.l,x
+sta $2122
+inx
+cpx #8
+bne -
+
+;We'll have two palettes, only one color is needed for the second:
+lda #33		;The color we need is the 33rd
+sta $2121
+lda.l Palette2
+sta $2122
+lda.l Palette2+1
+sta $2122
+ldx #UntitledData	; Address
+lda #:UntitledData	; of UntitledData
+ldy #(16*16*2)	; length of data
+stx $4302	; write
+sta $4304	; address
+sty $4305	; and length
+lda #%00000001	; set this mode (transferring words)
+sta $4300
+lda #$18	; $211[89]: VRAM data write
+sta $4301	; set destination
+
+ldy #$0000	; Write to VRAM from $0000
+sty $2116
+
+lda #%00000001	; start DMA, channel 0
+sta $420B
+lda #%10000000	; VRAM writing mode
+sta $2115
+ldx #$4000	; write to vram
+stx $2116	; from $4000
+
+;ugly code starts here - it writes the # shape I mentioned before.
+.rept 2
+   ;X|X|X
+   .rept 2
+     ldx #$0000	; tile 0 ( )
+     stx $2118
+     ldx #$0002	; tile 2 (|)
+     stx $2118
+   .endr
+   ldx #$0000
+   stx $2118
+   ;first line finished, add BG's
+   .rept 27
+     stx $2118	; X=0
+   .endr
+   ;beginning of 2nd line
+   ;-+-+-
+   .rept 2
+     ldx #$0004	; tile 4 (-)
+     stx $2118
+     ldx #$0006	; tile 6 (+)
+     stx $2118
+   .endr
+   ldx #$0004	; tile 4 (-)
+   stx $2118
+   ldx #$0000
+   .rept 27
+     stx $2118
+   .endr
+.endr
+.rept 2
+  ldx #$0000	; tile 0 ( )
+  stx $2118
+  ldx #$0002	; tile 2 (|)
+  stx $2118
+.endr
+ldx #$6000	; BG2 will start here
+stx $2116
+ldx #$000C	; And will contain 1 tile (cursor)
+stx $2118
+;set up the screen
+lda #%00110000	; 16x16 tiles, mode 0
+sta $2105	; screen mode register
+lda #%01000000	; data starts from $4000
+sta $2107	; for BG1
+lda #%01100000	; and $6000
+sta $2108	; for BG2
+
+stz $210B	; BG1 and BG2 use the $0000 tiles
+
+lda #%00000011	; enable bg1&2
+sta $212C
+
+;The PPU doesn't process the top line, so we scroll down 1 line.
+rep #$20	; 16bit a
+lda #$07FF	; this is -1 for BG1
+sep #$20	; 8bit a
+sta $210E	; BG1 vert scroll
+xba
+sta $210E
+
+rep #$20	; 16bit a
+lda #$FFFF	; this is -1 for BG2
+sep #$20	; 8bit a
+sta $2110	; BG2 vert scroll
+xba
+sta $2110
+
+lda #%00001111	; enable screen, set brightness to 15
+sta $2100
+
+lda #%10000001	; enable NMI and joypads
+sta $4200
+
+forever:
+wai
+rep #%00100000	; get 16 bit A
+lda #$0000		; empty it
+sep #%00100000	; 8 bit A
+lda $0100		; get our X coord
+ ConvertX		; WLA needs a space before a macro name
+sta $210F		; BG2 horz scroll
+xba
+sta $210F		; write 16 bits
+
+;now repeat it, but change $0100 to $0101, and $210F to $2110
+rep #%00100000	; get 16 bit A
+lda #$0000		; empty it
+sep #%00100000	; 8 bit A
+lda $0101		; get our Y coord
+ ConvertY		; WLA needs a space before a macro name
+sta $2110		; BG2 vert scroll
+xba
+sta $2110		; write 16 bits
+;--------------------------------------
+ldx #$0000		; reset our counter
+-
+rep #%00100000		; 16 bit A
+lda #$0000		; empty it
+sep #%00100000		; 8 bit a
+lda VRAMtable.l,x	; this is a long indexed address, nice :)
+rep #%00100000
+clc
+adc #$4000		; add $4000 to the value
+sta $2116		; write to VRAM from here
+lda #$0000		; reset A while it's still 16 bit
+sep #%00100000		; 8 bit A
+lda $0000,x		; get the corresponding tile from RAM
+; VRAM data write mode is still %10000000
+sta $2118		; write
+stz $2119		; this is the hi-byte
+inx
+cpx #9			; finished?
+bne -			; no, go back
+jmp forever
+
+;--------------------------------------
+.ends
+
+.bank 1 slot 0		; We'll use bank 1
+.org 0
+.section "Tiledata"
+.include "Scripts/tiles.inc"	; If you are using your own tiles, replace this
+.ends
+
+.bank 2 slot 0
+.org 0
+.section "Conversiontable"
+VRAMtable:
+.db $00,$02,$04,$40,$42,$44,$80,$82,$84
+.ends
